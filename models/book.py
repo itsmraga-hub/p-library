4807209275e60b8d2c0b6af19da44e2474453190
Raga <ragawilliam570@gmail.com>
2023-08-17 11:21:03 +0300

    Complete book class

--- models/book.py
+++ models/book.py
@@ -1,8 +1,12 @@
-from models.base_model import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 import uuid
+from sqlalchemy.orm import declarative_base
+from models.base_model import BaseModel, Base
 
 
-class Book(BaseModel):
-  name = ''
-  id = ''
-  code = ''
+class Book(BaseModel, Base):
+  __tablename__ = 'books'
+
+  name = Column(String)
+  code = Column(String)
+  user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
