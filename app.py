b615e9536a5604d46e1b0c18c8d59eb178576a33
Raga <ragawilliam570@gmail.com>
2023-08-17 14:43:48 +0300

    Add run logic to terminal program

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from colorama import Fore
 from manager.book_manager import BookManager
+from manager.user_manager import UserManager
+from models.engine import storage
 
 class App:
     def __init__(self, action):
         self._action = action.strip().lower()
         self.book_manager = BookManager()
+        self.user_manager = UserManager()
 
     @property
     def action(self):
@@ -14,22 +17,35 @@ class App:
     def action(self, action):
         self._action = action
 
-    def display(self):
+    def display_users(self):
+        print('[1] - list users')
+        print('[2] - Create User')
+
+    def display_books(self):
         print('[1] - List available books')
         print('[2] - Add books')
         print('[3] - Delete book')
         print('[4] - Update book')
         print('[5] - Sell book')
         print('[6] - Lend book')
-        self._action = input(Fore.GREEN + '>> ' + Fore.WHITE)
-        print(self._action)
+
+    def display(self):
+        self.display_users()
+        self._action = int(input(Fore.GREEN + '>> ' + Fore.WHITE))
 
 
     def manage(self):
         self.display()
+        # self.book_manager.list_books()
         try:
-            if self.action == '1':
-                self.book_manager.list_books()
+            if self.action == 1:
+                # self.book_manager.list_books()
+                self.user_manager.list_users()
+            elif self.action == 2:
+                self.user_manager.create_user()
+
+            self.display()
+
         except Exception as e:
             print(f'Failed - {e}')
 
