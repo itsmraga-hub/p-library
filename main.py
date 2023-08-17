a32e5af592dc323d9ea547e39cf8763e965d6bfe
Raga <ragawilliam570@gmail.com>
2023-08-17 11:18:30 +0300

    Create Main funtion to run the app

--- main.py
+++ main.py
@@ -1,49 +1,25 @@
-from models.user import User
-from models.book import Book
-from models.category import Category
+from app import App
+from colorama import Fore
+from models.engine import storage
 
 
-class Main:
-    def __init__(self):
-        pass
+def display():
+  print()
+  print(Fore.GREEN + 'Welcome to P-Library' + Fore.WHITE)
+  print()
+  print('Enter a command below to get started')
+  print('[C] - Create Account')
+  print('[L] - Log In')
+  print('')
+  print('Q - Quit')
+  print('')
 
-    def menu():
-        print('Welcome to P-Library')
-        print('Choose an option below:')
-        print('1: Add book Category')
-        print('2: Add book')
-        print('3: Add user')
-        print('4: Exit')
-        print()
-        Main.take_input()
-
-    def take_input():
-        arg = input('Enter choice: ')
-        print()
-
-    def run(self):
-        while True:
-            Main.menu()
+def main():
+  display()
+  action = input(Fore.GREEN + '>> ' + Fore.WHITE)
+  app = App(action)
+  app.run()
 
 
-m = Main()
-m.run()
-
-
-import sqlalchemy
-from sqlalchemy import create_engine
-
-
-HBNB_MYSQL_USER = 'raga'
-HBNB_MYSQL_PWD = 'root'
-HBNB_MYSQL_HOST = '127.0.0.1'
-HBNB_MYSQL_PORT = 5432
-HBNB_MYSQL_DB = 'p_library'
-engine = create_engine(f'postgresql://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@{HBNB_MYSQL_HOST}:{HBNB_MYSQL_PORT}/{HBNB_MYSQL_DB}')
-
-# Test the connection
-# try:
-#     with engine.connect() as connection:
-#         print("Connection successful!")
-# except Exception as e:
-#     print(f"Connection error: {str(e)}")
+if __name__ == '__main__':
+  main()
