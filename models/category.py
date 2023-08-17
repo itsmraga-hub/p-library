7f2a2394fc17c141df5fb6f5736cf4a38abffa1e
Raga <ragawilliam570@gmail.com>
2023-08-17 11:21:15 +0300

    complete Category class

--- models/category.py
+++ models/category.py
@@ -1,7 +1,10 @@
-from models.base_model import BaseModel
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String
+from models.base_model import BaseModel, Base
 
 
-class Category(BaseModel):
-  category_name = ''
-  category_number = 0
-  
+class Category(BaseModel, Base):
+  __tablename__ = 'categories'
+
+  category_name = Column(String)
+  category_number = Column(Integer)  
