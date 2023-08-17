bdac5a4eafa03e9351e69ba05fc64b8d16717e9d
Raga <ragawilliam570@gmail.com>
2023-08-17 11:18:10 +0300

    Create main app for App entry point

--- app.py
+++ app.py
@@ -1,38 +1,51 @@
-# import PySide6.QtCore
+from colorama import Fore
+from manager.book_manager import BookManager
 
-# # Prints PySide6 version
-# print(PySide6.__version__)
+class App:
+    def __init__(self, action):
+        self._action = action.strip().lower()
+        self.book_manager = BookManager()
 
-# # Prints the Qt version used to compile PySide6
-# print(PySide6.QtCore.__version__)
+    @property
+    def action(self):
+        return self._action
+
+    @action.setter
+    def action(self, action):
+        self._action = action
+
+    def display(self):
+        print('[1] - List available books')
+        print('[2] - Add books')
+        print('[3] - Delete book')
+        print('[4] - Update book')
+        print('[5] - Sell book')
+        print('[6] - Lend book')
+        self._action = input(Fore.GREEN + '>> ' + Fore.WHITE)
+        print(self._action)
 
 
-import sys
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton
-from PySide6.QtCore import Slot
+    def manage(self):
+        self.display()
+        try:
+            if self.action == '1':
+                self.book_manager.list_books()
+        except Exception as e:
+            print(f'Failed - {e}')
 
 
-# Greetings
-@Slot()
-def say_hello():
-    print("Button clicked, Hello!")
-
-
-def function():
-    print("The 'function' has been called!")
-
-app = QApplication(sys.argv)
-# Create a button
-button = QPushButton("Click me")
-# Connect the button to the function
-button.clicked.connect(say_hello)
-# label = QLabel("Hello World!")
-# This HTML approach will be valid too!
-# label2 = QLabel("<font color=red size=40>Hello World!</font>")
-# label.show()
-# label2.show()
-# Show the button
-button.show()
-# Run the main Qt loop
-# app.exec()
-app.exec()
+    def run(self):
+        try:
+            while True:
+                if self._action == 'q':
+                    print('bye')
+                    exit()
+                if self._action == 'l':
+                    print('Login')
+                    self.manage()
+                if self._action == 'c':
+                    print('Sign up')
+        except KeyboardInterrupt:
+            print()
+            print('bye')
+            exit()
